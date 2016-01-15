1b703f68f05a107a2c89d92f53b7c34442039b8f
RosarioACali <alexcali@outlook.it>
2016-01-14 20:59:39 -0500

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,18 @@
+//Modular Programming Example
+//main.cpp
+
+#include "main.h"
+#include "Transaction.h"
+
+int main()
+{
+    int i;
+    struct Transaction tr;
+
+    for (i = 0; i < NO_TRANSACTIONS; i++) {
+        enter(&tr);
+        display(&tr);
+    }
+
+    return 0;
+}
